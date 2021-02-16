4276a02fb6e0f9c94c8f0639db9c48ec2259781f
SiriusPluge <78333450+SiriusPluge@users.noreply.github.com>
2021-02-16 21:38:50 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+import time
+
+
+host = '192.168.0.11'
+port = 9090
+
+
+clients = []
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+
+
+quit = False
+print('[ Server Started ]')
+
+
+while not quit:
+    try:
+        data, addr = s.recvfrom(1024)
+
+
+        if addr not in clients:
+            clients.append(addr)
+
+
+        itsatime = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+
+
+        print("['+addr[0]+']=['+addr[1]']=['+str(addr[1])+']=['+itsatime+']/", end='')
+        print(data.decode('utf-8'))
+
+
+        for client in clients:
+            if addr != clients:
+                s.sendto(data, client)
+
+
+    except:
+        print('\n[ Server Stopped ]')
+        quit = True
+
+
+s.close()
